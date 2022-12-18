4cb648b3f53ba4dadc55021f1f519375377b1b72
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-18 13:00:17 -0800

    Working on Cyrillic-Latin translit.

--- app/views.py
+++ app/views.py
@@ -10,6 +10,47 @@ def index(request):
 def transliterate(request):
     if request.method == 'POST':
         inputText = request.POST['inputText']
+        outputText = ""
+
+        transcriptions = {
+            "А": "A",
+            "Б": "B",
+            "В": "V",
+            "Г": "G",
+            "Д": "D",
+            "Е": "YE",
+            "Ё": "YO",
+            "Ж": "ZH",
+            "З": "Z",
+            "И": "I",
+            "Й": "Y",
+            "К": "K",
+            "Л": "L",
+            "М": "M",
+            "Н": "N",
+            "О": "O",
+            "П": "P",
+            "Р": "R",
+            "С": "S",
+            "Т": "T",
+            "У": "U",
+            "Ф": "F",
+            "Х": "H",
+            "Ц": "TS",
+            "Ч": "CH",
+            "Ш": "SH",
+            "Щ": "SHCH",
+            "Ъ": "",
+            "Ы": "Y",
+            "Ь": "\'",
+            "Э": "E",
+            "Ю": "YU",
+            "Я": "YA",
+        }
+
+        # for each char in text, convert
+        for char in inputText:
+            outputText += transcriptions[char]
 
         # inputText is being used for testing; temporary
-        return render(request, 'result.html', {'outputText': inputText})
+        return render(request, 'result.html', {'outputText': outputText})
