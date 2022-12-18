0e1aa8acdb97a22e6e66d51981fa9f548768c74d
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-17 20:25:08 -0800

    Added result page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-  return render(request, 'index.html')
+    return render(request, 'index.html')
+
+
+def transliterate(request):
+    if request.method == 'POST':
+        inputText = request.POST['inputText']
+
+        # inputText is being used for testing; temporary
+        return render(request, 'result.html', {'outputText': inputText})
